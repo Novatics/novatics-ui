fcd87549f37d488ab445d523e3e6beec2aba4d38
Matheus Gonçalves <souzagonc@gmail.com>
2022-06-06 22:20:07 -0300

    add yarn scripts

--- packages/novatics-logo/src/lib/novatics-logo.tsx
+++ packages/novatics-logo/src/lib/novatics-logo.tsx
@@ -1,17 +1,35 @@
-import styled from '@emotion/styled';
-
-/* eslint-disable-next-line */
-export interface NovaticsLogoProps {}
-
-const StyledNovaticsLogo = styled.div`
-  color: pink;
-`;
+export interface NovaticsLogoProps {
+  size?: number;
+  variant?: string;
+  title?: string;
+}
 
 export function NovaticsLogo(props: NovaticsLogoProps) {
+  const { title = 'Novatics', size = 300, variant, ...rest } = props;
+
   return (
-    <StyledNovaticsLogo>
-      <h1>Welcome to NovaticsLogo!</h1>
-    </StyledNovaticsLogo>
+    <svg
+      width={size}
+      height={size}
+      viewBox="0 0 421 400"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...rest}
+    >
+      <title>{title}</title>
+      <path
+        d="M210.595 53.9897V170.677L315.167 287.399V112.386L210.595 53.9897Z"
+        fill="#FFCC00"
+      />
+      <path
+        d="M210.595 229.026L315.167 287.352L210.595 170.677V229.026Z"
+        fill="#FF9D00"
+      />
+      <path
+        d="M106 112.386V287.352L210.595 345.748V229.026V170.63L106 112.386Z"
+        fill="#832AF5"
+      />
+    </svg>
   );
 }
 

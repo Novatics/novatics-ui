153e5a17bc29deec6e57221608b5e4caaebe3aa5
Matheus Gonçalves <souzagonc@gmail.com>
2022-06-23 20:03:05 -0300

    feat(root): setup for commitlint

---
+++ .cz-config.js
@@ -0,0 +1,56 @@
+module.exports = {
+  types: [
+    { value: 'feat', name: 'feat: A new feature' },
+    { value: 'fix', name: 'fix: A bug fix' },
+    { value: 'docs', name: 'docs: Documentation only changes' },
+    {
+      value: 'style',
+      name: 'style: Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)',
+    },
+    {
+      value: 'refactor',
+      name: 'refactor: A code change that neither fixes a bug nor adds a feature',
+    },
+    { value: 'test', name: 'test: Adding missing tests' },
+    {
+      value: 'chore',
+      name: 'chore: Changes to the build process or auxiliary tools and libraries such as documentation generation',
+    },
+    { value: 'WIP', name: 'WIP: Work in progress' },
+  ],
+
+  scopes: [
+    { name: 'root' },
+    { name: 'release' },
+    { name: 'button' },
+    { name: 'core' },
+  ],
+
+  allowTicketNumber: false,
+  isTicketNumberRequired: false,
+  ticketNumberPrefix: 'TICKET-',
+  ticketNumberRegExp: 'd{1,5}',
+
+  // it needs to match the value for field type. Eg.: 'fix'
+  /*
+    scopeOverrides: {
+      fix: [
+        {name: 'merge'},
+        {name: 'style'},
+        {name: 'e2eTest'},
+        {name: 'unitTest'}
+      ]
+    },
+    */
+  // override the messages, defaults are as follows
+  allowCustomScopes: true,
+  allowBreakingChanges: ['feat', 'fix'],
+  // skip any questions you want
+  skipQuestions: ['body'],
+
+  // limit subject length
+  subjectLimit: 100,
+  // breaklineChar: '|', // It is supported for fields body and footer.
+  // footerPrefix : 'ISSUES CLOSED:'
+  // askForBreakingChangeFirst : true, // default is false
+};

258a01235d6e4a0c214fdad9582e3ead88d01186
Fernando Lacerda <fernando.a.lacerda@gmail.com>
2022-07-15 15:35:59 -0300

    wip2

--- packages/currency-text-field/src/lib/currency-text-field.stories.tsx
+++ packages/currency-text-field/src/lib/currency-text-field.stories.tsx
@@ -10,7 +10,12 @@ export default {
 } as Meta;
 
 const Template: Story<CurrencyTextFieldProps> = (args) => (
-  <CurrencyTextField {...args} />
+  <CurrencyTextField
+    value={10}
+    onChange={(value: String) => {
+      console.log('value',value);
+    }}
+  />
 );
 
 export const Primary = Template.bind({});

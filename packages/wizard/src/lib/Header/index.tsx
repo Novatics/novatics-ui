b37e6d2b0082150ff0173736e2a0e1fc187b19c7
Vanderlei Chagas <vanderleichagas42@gmail.com>
2022-08-03 16:42:05 -0300

    feat(wizard): add title and subtitle to header

--- packages/wizard/src/lib/Header/index.tsx
+++ packages/wizard/src/lib/Header/index.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 
 interface HeaderProps {
-  step?: number;
+  step: number;
+  title?: string;
+  subtitle?: string
 }
 
-const Header = ({ step = 1 }: HeaderProps) => {
+const Header = ({ step, title = `STEP ${step} Template`, subtitle}: HeaderProps) => {
   return (
     <div>
-      <h2>STEP {step} Template</h2>
+      <h2>{title}</h2>
+      <h4>{subtitle}</h4>
     </div>
   );
 };

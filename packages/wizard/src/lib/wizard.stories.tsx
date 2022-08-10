3063d2a76f6d0aa2cba77a3530a94c140f47a1bd
Vanderlei Chagas <vanderleichagas42@gmail.com>
2022-08-10 17:48:45 -0300

    feat(wizard): allow component on tab content

--- packages/wizard/src/lib/wizard.stories.tsx
+++ packages/wizard/src/lib/wizard.stories.tsx
@@ -1,13 +1,69 @@
+import {useState} from 'react';
 import { Meta, Story } from '@storybook/react';
-import React from 'react';
 import Wizard, { WizardProps } from './wizard';
-
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+//Todo: Test if it works with a form
 export default {
   component: Wizard,
   title: 'Wizard',
 } as Meta;
 
-const Template: Story<WizardProps> = (args) => <Wizard {...args} />;
+
+
+
+ function NameForm({form, setForm}: any) {
+  return (
+    <Box>
+      <TextField id="name"
+                label="name"
+                variant="outlined"
+                 onChange={(e) => setForm({...form, name: e.target.value})}
+                 value={form.name}  />
+    </Box>
+  )
+}
+
+
+function Form() {
+  const [form, setForm] = useState({});
+
+  const steps = [
+    {
+      label: 'Select campaign settings',
+      content: `For each ad campaign that you create, you can control how much
+                you're willing to spend on clicks and conversions, which networks
+                and geographical locations you want your ads to show on, and more.`,
+    },
+    {
+      label: 'Create an ad group',
+      content: <NameForm form={form} setForm={setForm}/>
+    },
+    {
+      label: 'Create an ad',
+      content: `Try out different ad text to see what brings in the most customers,
+                and learn how to enhance your ads using features like ad extensions.
+                If you run into any problems with your ads, find out how to tell if
+                they're running and how to resolve approval issues.`,
+    },
+  ];
+
+  const onBack = (currentStep: number) => {
+    console.log(currentStep, form)
+  };
+
+  const onNext = (currentStep: number) => {
+    console.log(currentStep, form)
+  };
+
+    return <Wizard onNext={onNext} onBack={onBack} steps={steps} />
+
+}
+
+
+const Template: Story<WizardProps> = (args) => {
+  return <Form/>
+};
 
 const onBack = () => {
   // onBack stuff

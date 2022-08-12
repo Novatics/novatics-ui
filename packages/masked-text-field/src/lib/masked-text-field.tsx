d6c60305395beff1a236e7f9917857bb1f58dd60
Thiago Pereira <mr.thiagoo@gmail.com>
2022-08-12 13:49:53 -0400

    dojo2: add initial presets

--- packages/masked-text-field/src/lib/masked-text-field.tsx
+++ packages/masked-text-field/src/lib/masked-text-field.tsx
@@ -1,16 +1,29 @@
-import { TextField } from '@mui/material';
+import { JSX } from '@emotion/react/jsx-runtime';
+import { TextField, TextFieldProps } from '@mui/material';
 import InputMask from "react-input-mask";
+
+
+// Presets:
+
+// CPF/CNPJ (999.999.999-99 | 99.999.999/9999-99)
+// CPF (999.999.999-99)
+// CNPJ (99.999-999)
+// CEP (99.999-999)
+// TELEFONE (+99 (99) 99999-9999)
+// PLACA (AAA-9999) *
+// PLACA (AAA-AAAA) *
+// CARTAO (9999 9999 9999 9999)
 
 
 /* eslint-disable-next-line */
 export interface MaskedTextFieldProps {
   value: string | number;
-  mask: string;
-  maskPlaceholder: string;
+  mask: 'cpf' | 'cnpj' | 'cpf-cnpj' | 'cep' | 'zipcode' | 'phone' | 'card-number';
+  placeholder: string;
 }
 
 export function MaskedTextField(props: MaskedTextFieldProps) {
-  const { value } = props;
+  const { value, mask, placeholder, ...rest } = props;
 
   const handleOnChangeEvent = (value: string | number) => {
     console.log("value => ", value);
@@ -22,7 +35,8 @@ export function MaskedTextField(props: MaskedTextFieldProps) {
       value={value}
       onChange={(event) => handleOnChangeEvent(event.target.value)}
     >
-      { (inputProps) => <TextField {...inputProps } /> }
+      { (inputProps: JSX.IntrinsicAttributes & JSX.LibraryManagedAttributes<(props: TextFieldProps) => JSX.Element, TextFieldProps>) => <TextField {...inputProps } /> }
+
     </InputMask>
   );
 }

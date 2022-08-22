146788f05ca6dfd05685111d5170bde69c8796db
Raphael Cunha <raphael.s.cunha@gmail.com>
2022-08-22 20:41:32 -0300

    feat(root): setup tokens

---
+++ packages/tokens/src/lib/tokens.ts
@@ -0,0 +1,7 @@
+import colors from './colors.json';
+import radius from './radius.json';
+import shadows from './shadows.json';
+import spacing from './spacing.json';
+import typography from './typography.json';
+
+export { colors, radius, shadows, spacing, typography };

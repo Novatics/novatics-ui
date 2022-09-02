ddcb26be686f79104b3bed11c614696bca2af108
Vanderlei Chagas <vanderleichagas42@gmail.com>
2022-09-02 16:30:37 -0300

    feat(wizard): add subtitle to tab

--- packages/wizard/src/lib/Tab/index.tsx
+++ packages/wizard/src/lib/Tab/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import BaseTab from '@mui/material/Tab';
 import CheckIcon from '@mui/icons-material/Check';
-
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 interface Step {
   label: string;
+  subtitle?: string;
   content: React.ReactNode | string;
 }
 
@@ -19,17 +22,20 @@ const Tab = (props : TabProps) => {
   const hasTabComponent = !!TabComponent;
 
   return (
-
     hasTabComponent ? <TabComponent {...props} /> :
-    <> <BaseTab label={step.label} />
-          {isComplete && showCompleted && (
+        <Button variant="text" sx={{maxWidth: '350px'}}>
+            <Box sx={{display: 'flex', flexDirection: 'column', alignItems: 'flex-start', flexWrap: 'wrap', maxWidth: '350px'}} >
+              <Typography variant="body1">{step.label && step.label}</Typography>
+              <Typography variant="subtitle2">{step.subtitle && step.subtitle}</Typography>
+            </Box>
+            {isComplete && showCompleted && (
             <CheckIcon
               style={{ marginLeft: 'auto', marginRight: '10px' }}
               fontSize="small"
               color="success"
             />
           )}
-    </>
+        </Button>
   );
 };
 

89ec91c20de05d9ae6943c0095f8d3b9024d03ce
Vanderlei Chagas <vanderleichagas42@gmail.com>
2022-09-14 16:31:16 -0300

    feat(wizard): disabled tab

--- packages/wizard/src/lib/Tab/styles.ts
+++ packages/wizard/src/lib/Tab/styles.ts
@@ -1,15 +1,18 @@
 import styled from '@emotion/styled';
 import Box from '@mui/material/Box';
 
-export const Container = styled(Box)`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    width: 110%;
-`;
+export const Container = styled(Box) (({ disabled }) => ({
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    width: '100%',
+    cursor:  disabled ? 'no-drop' : 'cursor',
+  }))
+
 
 export const TextContainer = styled(Box)`
     display: flex;
     flex-direction: column;
     align-items: flex-start;
 `
+

0ee23c8a6ed68275504556cb08ec286483999546
felipebayres <felipebayres@gmail.com>
2022-10-21 15:47:40 -0300

    feat(root): feat: add list to the component

--- packages/time-picker/src/lib/clock-picker.tsx
+++ packages/time-picker/src/lib/clock-picker.tsx
@@ -1,11 +1,15 @@
 import { ClockPickerProps } from '@mui/x-date-pickers';
-
+import {List, ListItem} from '@mui/material'
 interface Props extends ClockPickerProps<Date> {
   oi?: string;
 }
 
 const ClockPicker = (props: Props) => {
-  return <div>oi</div>;
+  return <div>
+    <List>
+  {Array(24).fill(0).map((_, i) => <ListItem>{i+1}</ListItem>)}
+
+    </List></div>;
 };
 
 export default ClockPicker;

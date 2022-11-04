d62e2edad326284186d6fd78e638b1049eb56562
Matheus Gonçalves <souzagonc@gmail.com>
2022-11-04 19:00:49 -0300

    feat(root): tesing

--- packages/currency-text-field/src/lib/currency-text-field.tsx
+++ packages/currency-text-field/src/lib/currency-text-field.tsx
@@ -42,7 +42,7 @@ export function CurrencyTextField(props: CurrencyTextFieldProps) {
     currencyProps,
     ...rest
   } = props;
-  const [maskedValue, setMaskedValue] = useState('1');
+  const [maskedValue, setMaskedValue] = useState('3');
 
   const priorityCurrencyProps = useMemo(() => {
     const currencyFallback = pick(CURRENCY['brl'], CURRENCY_ATTRIBUTES);

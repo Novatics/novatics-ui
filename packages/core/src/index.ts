c308f0690b089d3aded032ec8a7bbab55a454d46
Matheus Gonçalves <souzagonc@gmail.com>
2022-12-08 11:28:54 -0300

    fix(wizard): fix wizard path and update packages

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -3,3 +3,4 @@ export * from '@novatics/currency-text-field';
 export * from '@novatics/styles';
 export { shadows, borderRadius, typography } from '@novatics/styles';
 export * from '@novatics/tokens';
+export * from '@novatics/wizard';

ab8a40c31c8a26385737cd21b0894c2b20361106
gsanto <espiritosanto.gabriel@gmail.com>
2023-02-03 15:19:57 -0300

    feat(checkbox): updated checkbox indeterminate icon

--- packages/checkbox/src/lib/checkbox.tsx
+++ packages/checkbox/src/lib/checkbox.tsx
@@ -1,98 +1,66 @@
-import { Buffer } from 'buffer';
-
 import MuiCheckbox, { CheckboxProps as MuiCheckboxProps } from '@mui/material/Checkbox';
-import { FormControlLabelProps } from '@mui/material/FormControlLabel';
 
-import { styled } from '@mui/material/styles';
+import { styled } from '@novatics/styles';
 
 
-const CheckboxIconSvg = (color?: string) => `<svg width="12" height="8" viewBox="0 0 12 8" fill="none" xmlns="http://www.w3.org/2000/svg">
-  <path d="M4.75776 7.74112L11.0244 1.47445L9.77998 0.230011L4.75776 5.25223L2.22443 2.7189L0.97998 3.96334L4.75776 7.74112Z" fill="${color || "#fff"}" />
-</svg>`;
-
-export interface CheckboxProps extends FormControlLabelProps {
-  CheckboxProps: MuiCheckboxProps;
-  disabled?: boolean;
+/* eslint-disable-next-line */
+export interface CheckboxProps extends MuiCheckboxProps {
+  variant: 'filled' | 'outlined';
 }
 
-const BpIcon = styled('span')(({ theme }) => ({
-  borderRadius: 3,
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const IndeterminateIcon = styled('span')<{ variant: 'filled' | 'outlined'; }>(({ theme, variant }: { theme: any, variant: 'filled' | 'outlined'; }) => ({
   width: 16,
   height: 16,
-  boxShadow:
-    theme.palette.mode === 'dark'
-      ? '0 0 0 1px rgb(16 22 26 / 40%)'
-      : 'inset 0 0 0 1px rgba(16,22,26,.2), inset 0 -1px 0 rgba(16,22,26,.1)',
-  backgroundColor: theme.palette.mode === 'dark' ? '#394b59' : '#f5f8fa',
-  backgroundImage:
-    theme.palette.mode === 'dark'
-      ? 'linear-gradient(180deg,hsla(0,0%,100%,.05),hsla(0,0%,100%,0))'
-      : 'linear-gradient(180deg,hsla(0,0%,100%,.8),hsla(0,0%,100%,0))',
-  '.Mui-focusVisible &': {
-    outline: '2px auto rgba(19,124,189,.6)',
-    outlineOffset: 2,
+  backgroundColor: variant === 'filled' ? theme.palette.primary.main : 'transparent',
+  borderRadius: '2px',
+  border: variant === 'filled' ? `2px solid ${theme.palette.primary.main}` : `2px solid ${theme.palette.primary.main}`,
+
+  '&:before': {
+    // center content to create line in the middle
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+
+    display: 'block',
+    width: 8.89,
+    height: 1.78,
+    content: '""',
+    backgroundColor: variant === 'filled' ? theme.palette.grayScale.supernova : theme.palette.primary.main,
   },
+
   'input:hover ~ &': {
-    backgroundColor: theme.palette.mode === 'dark' ? '#30404d' : '#ebf1f5',
+    backgroundColor: variant === 'filled' ? theme.palette.primary.dark : theme.palette.primary.light,
+    borderColor: theme.palette.primary.dark,
+
+    '&:before': {
+      backgroundColor: variant === 'filled' ? theme.palette.grayScale.supernova : theme.palette.primary.medium,
+    }
   },
+
   'input:disabled ~ &': {
-    boxShadow: 'none',
-    background:
-      theme.palette.mode === 'dark' ? 'rgba(57,75,89,.5)' : 'rgba(206,217,224,.5)',
+    backgroundColor: variant === 'filled' ? theme.palette.grayScale.spaceStation : 'transparent',
+    border: variant === 'filled' ? `2px solid ${theme.palette.grayScale.spaceStation}` : `2px solid ${theme.palette.grayScale.spaceStation}`,
+
+    '&:before': {
+      backgroundColor: variant === 'filled' ? theme.palette.grayScale.supernova : theme.palette.grayScale.spaceStation,
+    }
   },
 }));
 
-const BpCheckedIcon = styled(BpIcon)({
-  backgroundColor: '#137cbd',
-  backgroundImage: 'linear-gradient(180deg,hsla(0,0%,100%,.1),hsla(0,0%,100%,0))',
-  '&:before': {
-    display: 'block',
-    width: 16,
-    height: 16,
-    backgroundImage:
-      `url(data:image/svg+xml;base64,${Buffer.from(CheckboxIconSvg()).toString('base64')})`,
-    backgroundRepeat: 'no-repeat',
-    backgroundSize: 'contain',
-    backgroundPosition: 'center',
-    content: '""',
-  },
-  'svg': {
-    'path': {
-      fill: '#000',
-    },
-  },
-  'input:hover ~ &': {
-    backgroundColor: '#106ba3',
-  },
-});
-
-// Inspired by blueprintjs
-function BpCheckbox(props: CheckboxProps) {
-  return (
-    <MuiCheckbox
-      sx={{
-        '&:hover': { bgcolor: 'transparent' },
-      }}
-      disableRipple
-      color="default"
-      checkedIcon={<BpCheckedIcon />}
-      icon={<BpIcon />}
-      inputProps={{ 'aria-label': 'Checkbox demo' }}
-      {...props}
-    />
-  );
-}
-
-// TODO Label will always be to the right of the checkbox
-export function Checkbox({ CheckboxProps, ...props }: CheckboxProps) {
-  return (
-    <div>
-      <BpCheckbox />
-      <BpCheckbox defaultChecked />
-      <BpCheckbox disabled />
-      <MuiCheckbox />
-    </div>
-  );
+export function Checkbox({ variant = "filled", ...other }: CheckboxProps) {
+  return <MuiCheckbox
+    color="default"
+    disableFocusRipple
+    disableRipple
+    disableTouchRipple
+    indeterminateIcon={<IndeterminateIcon variant={variant} />}
+    // icon={<RadioIcon error={error} />}
+    // checkedIcon={<RadioCheckedIcon error={error} />}
+    // sx={deepmerge({ '&:hover': { backgroundColor: 'transparent' } }, sx)}
+    {...other}
+  />;
 }
 
 export default Checkbox;

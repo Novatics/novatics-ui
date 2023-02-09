d26c3aea6ddf84f68be0245f92cf890be68731d7
gsanto <espiritosanto.gabriel@gmail.com>
2023-02-09 13:44:11 -0300

    feat(checkbox): updated checkbox with extensible colors

--- packages/checkbox/src/lib/checkbox.tsx
+++ packages/checkbox/src/lib/checkbox.tsx
@@ -3,19 +3,21 @@ import CheckIcon from '@mui/icons-material/Check';
 import { styled } from '@novatics/styles';
 
 type PropsToOmit = 'disableFocusRipple' | 'centerRipple' | 'disableTouchRipple' | 'focusRipple' | 'TouchRippleProps' | 'touchRippleRef';
+type Color = 'primary' | 'secondary' | 'error' | 'info' | 'success' | 'warning' | 'default';
 
 export interface CheckboxProps extends Omit<MuiCheckboxProps, PropsToOmit> {
   variant: 'filled' | 'outlined';
-  error?: boolean;
+  color?: Color;
+  // error?: boolean;
 }
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-const IndeterminateIcon = styled('span')<{ variant: 'filled' | 'outlined'; }>(({ theme, variant }: { theme: any, variant: 'filled' | 'outlined'; }) => ({
+const IndeterminateIcon = styled('span')<{ colorType: Color; variant: 'filled' | 'outlined'; }>(({ colorType, theme, variant }: { colorType: Color, theme: any, variant: 'filled' | 'outlined'; }) => ({
   width: 14,
   height: 14,
-  backgroundColor: variant === 'filled' ? theme.palette.primary.main : 'transparent',
+  backgroundColor: variant === 'filled' ? theme.palette[colorType].main : 'transparent',
   borderRadius: '2px',
-  border: variant === 'filled' ? `2px solid ${theme.palette.primary.main}` : `2px solid ${theme.palette.primary.main}`,
+  border: variant === 'filled' ? `2px solid ${theme.palette[colorType].main}` : `2px solid ${theme.palette[colorType].main}`,
 
   '&:before': {
     // center content to create line in the middle
@@ -28,15 +30,15 @@ const IndeterminateIcon = styled('span')<{ variant: 'filled' | 'outlined'; }>(({
     width: 8.89,
     height: 1.78,
     content: '""',
-    backgroundColor: variant === 'filled' ? theme.palette.grayScale.supernova : theme.palette.primary.main,
+    backgroundColor: variant === 'filled' ? theme.palette.grayScale.supernova : theme.palette[colorType].main,
   },
 
   'input:hover ~ &': {
-    backgroundColor: variant === 'filled' ? theme.palette.primary.dark : theme.palette.primary.light,
-    borderColor: theme.palette.primary.dark,
+    backgroundColor: variant === 'filled' ? theme.palette[colorType].dark : theme.palette[colorType].light,
+    borderColor: theme.palette[colorType].dark,
 
     '&:before': {
-      backgroundColor: variant === 'filled' ? theme.palette.grayScale.supernova : theme.palette.primary.medium,
+      backgroundColor: variant === 'filled' ? theme.palette.grayScale.supernova : theme.palette[colorType].medium ? theme.palette[colorType].medium : theme.palette[colorType].dark,
     }
   },
 
@@ -50,7 +52,7 @@ const IndeterminateIcon = styled('span')<{ variant: 'filled' | 'outlined'; }>(({
   },
 }));
 
-const UncheckedIcon = styled(CheckIcon)<{ error?: boolean; }>(({ theme, error }: { theme: any; error?: boolean; }) => ({
+const UncheckedIcon = styled(CheckIcon)<{ colorType: Color; error?: boolean; }>(({ colorType, theme, error }: { colorType: Color; theme: any; error?: boolean; }) => ({
   width: 14,
   height: 14,
   borderRadius: '2px',
@@ -58,33 +60,33 @@ const UncheckedIcon = styled(CheckIcon)<{ error?: boolean; }>(({ theme, error }:
   // Set color of the check icon
   color: 'transparent',
 
-  'input:hover ~ &': !error && {
-    backgroundColor: theme.palette.primary.light,
-    borderColor: theme.palette.primary.dark,
+  'input:hover ~ &': {
+    backgroundColor: theme.palette[colorType].light,
+    borderColor: theme.palette[colorType].dark,
     // Set color of the check icon
-    color: theme.palette.primary.medium,
+    color: theme.palette[colorType].medium ? theme.palette[colorType].medium : theme.palette[colorType].dark,
   },
 }));
 
-const CheckedIcon = styled(UncheckedIcon)<{ variant: 'filled' | 'outlined'; disabled?: boolean; }>(({ theme, variant, disabled }: { theme: any; variant?: 'filled' | 'outlined'; disabled?: boolean; }) => {
+const CheckedIcon = styled(UncheckedIcon)<{ colorType: Color; variant: 'filled' | 'outlined'; disabled?: boolean; }>(({ colorType, theme, variant, disabled }: { colorType: Color; theme: any; variant?: 'filled' | 'outlined'; disabled?: boolean; }) => {
   const getColor = () => {
     if (disabled && variant === 'outlined') return theme.palette.grayScale.spaceStation;
     if (disabled && variant === 'filled') return theme.palette.grayScale.supernova;
     if (variant === 'filled') return theme.palette.grayScale.supernova;
-    return theme.palette.primary.main;
+    return theme.palette[colorType].main;
   };
 
   const getBorderColor = () => {
     if (disabled && variant === 'outlined') return theme.palette.grayScale.spaceStation;
     if (disabled && variant === 'filled') return theme.palette.grayScale.spaceStation;
-    if (variant === 'filled') return theme.palette.primary.main;
-    return theme.palette.primary.main;
+    if (variant === 'filled') return theme.palette[colorType].main;
+    return theme.palette[colorType].main;
   };
 
   const getBackgroundColor = () => {
     if (disabled && variant === 'outlined') return theme.palette.grayScale.supernova;
     if (disabled && variant === 'filled') return theme.palette.grayScale.spaceStation;
-    if (variant === 'filled') return theme.palette.primary.main;
+    if (variant === 'filled') return theme.palette[colorType].main;
     return theme.palette.grayScale.supernova;
   };
 
@@ -94,23 +96,23 @@ const CheckedIcon = styled(UncheckedIcon)<{ variant: 'filled' | 'outlined'; disa
     borderColor: getBorderColor(),
 
     'input:hover ~ &': {
-      backgroundColor: disabled ? getBackgroundColor() : theme.palette.primary.light,
-      borderColor: disabled ? getBorderColor() : theme.palette.primary.dark,
+      backgroundColor: disabled ? getBackgroundColor() : theme.palette[colorType].light,
+      borderColor: disabled ? getBorderColor() : theme.palette[colorType].dark,
       color: disabled ? getColor() : theme.palette.grayScale.medium,
     },
   });
 });
 
-export function Checkbox({ variant = "filled", error, disabled, ...other }: CheckboxProps) {
+export function Checkbox({ variant = "filled", color = "primary", disabled, ...other }: CheckboxProps) {
   return <MuiCheckbox
     color="default"
     disableFocusRipple
     disableRipple
     disableTouchRipple
     disabled={disabled}
-    icon={<UncheckedIcon error={error} />}
-    checkedIcon={<CheckedIcon variant={variant} disabled={disabled} />}
-    indeterminateIcon={<IndeterminateIcon variant={variant} />}
+    icon={<UncheckedIcon colorType={color} error={color === 'error'} />}
+    checkedIcon={<CheckedIcon colorType={color} variant={variant} disabled={disabled} />}
+    indeterminateIcon={<IndeterminateIcon colorType={color} variant={variant} />}
     {...other}
   />;
 }

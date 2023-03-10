03144b2710dfd6d1bd0c337c66342e2aa6da6a70
JL Yamin <jlyaminc@gmail.com>
2023-03-10 17:11:06 -0300

    wip: [NUI-86] WIP: Created component and started styling

--- packages/styles/src/lib/Theme/palette.ts
+++ packages/styles/src/lib/Theme/palette.ts
@@ -22,9 +22,11 @@ declare module '@mui/material/styles/createPalette' {
   interface PaletteOptions {
     grayScale?: GrayScale;
     tertiary?: SimplePaletteColorOptions;
+    support?: SimplePaletteColorOptions;
   }
   interface Palette {
     grayScale: GrayScale;
+    support: SimplePaletteColorOptions;
   }
   interface TypeObject {
     grayScale: GrayScale;
@@ -81,6 +83,10 @@ export const palette: PaletteOptions = {
     main: colors['mars'],
     dark: colors['mars--dark'],
     light: colors['mars--light'],
+  },
+  support: {
+    main: colors['universe'],
+    light: colors['universe--light'],
   },
 };
 

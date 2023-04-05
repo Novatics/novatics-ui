cc841e0783e930fbcf714c10b5c237f84ba6466e
Matheus Gonçalves <souzagonc@gmail.com>
2023-04-05 20:51:24 -0300

    feat(divider): implement divider

--- packages/styles/src/lib/Theme/components/MuiDivider.ts
+++ packages/styles/src/lib/Theme/components/MuiDivider.ts
@@ -1,5 +1,32 @@
-import { Components } from '@mui/material';
+import { Components, Theme } from '@mui/material';
 
 export const MuiDivider: Components['MuiDivider'] = {
-  styleOverrides: {},
+  styleOverrides: {
+    textAlignLeft: {
+      '::before': {
+        width: '24px',
+      },
+    },
+    textAlignRight: {
+      '::after': {
+        width: '24px',
+      },
+    },
+    wrapper: (props) => {
+      const theme = props.theme as Theme;
+      return {
+        padding: '4px 8px',
+        color: theme.palette.grey[50],
+        ...theme.typography.overline,
+      };
+    },
+    wrapperVertical: (props) => {
+      const theme = props.theme as Theme;
+      return {
+        padding: '8px 4px',
+        color: theme.palette.grey[50],
+        ...theme.typography.overline,
+      };
+    },
+  },
 };

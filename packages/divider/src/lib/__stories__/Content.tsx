c46cb39ec6297da0ea2181cf991133f022837eff
Matheus Gonçalves <souzagonc@gmail.com>
2023-04-19 17:38:31 -0300

    fix(divider): fix divider story

---
+++ packages/divider/src/lib/__stories__/Content.tsx
@@ -0,0 +1,9 @@
+import { Typography } from '@mui/material';
+
+export const Content = () => (
+  <Typography>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus id
+    dignissim justo. Nulla ut facilisis ligula. Interdum et malesuada fames ac
+    ante ipsum primis in faucibus. Sed malesuada lobortis pretium.
+  </Typography>
+);

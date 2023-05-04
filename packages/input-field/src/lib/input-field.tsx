5c1105ab1e67fe6aac96121f252af19b1c580444
Matheus Gonçalves <souzagonc@gmail.com>
2023-05-04 20:46:50 -0300

    feat(input-field): generate component

---
+++ packages/input-field/src/lib/input-field.tsx
@@ -0,0 +1,7 @@
+import MuiOutlinedInput, { OutlinedInputProps } from '@mui/material/OutlinedInput';
+
+const InputField = (props: OutlinedInputProps) => <MuiOutlinedInput {...props} />;
+
+export { InputField };
+export default InputField;
+export * from '@mui/material/OutlinedInput';

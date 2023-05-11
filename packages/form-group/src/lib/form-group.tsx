257b2d411272d61b4924b26988dc3dc7abdaed62
JL Yamin <jlyaminc@gmail.com>
2023-05-11 16:55:14 -0300

    wip: added spacings

--- packages/form-group/src/lib/form-group.tsx
+++ packages/form-group/src/lib/form-group.tsx
@@ -1,6 +1,27 @@
-import MuiFormGroup, { FormGroupProps } from '@mui/material/FormGroup';
+import { Typography } from '@mui/material';
+import MuiFormGroup, {
+  FormGroupProps as MUIFormGroupProps,
+} from '@mui/material/FormGroup';
 
-const FormGroup = (props: FormGroupProps) => <MuiFormGroup {...props} />;
+export interface FormGroupProps extends MUIFormGroupProps {
+  title?: string;
+  direction: 'row' | 'column';
+}
+
+const FormGroup = (props: FormGroupProps) => {
+  const { title, direction = 'column' } = props;
+
+  return (
+    <>
+      {title && (
+        <Typography sx={{ mb: 2.5 }} variant="h5" fontWeight="500">
+          {title}
+        </Typography>
+      )}
+      <MuiFormGroup sx={{ gap: 2.5 }} row={direction === 'row'} {...props} />
+    </>
+  );
+};
 
 export { FormGroup };
 export default FormGroup;

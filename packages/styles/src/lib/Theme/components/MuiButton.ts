03d3832d100ff6e70ffc0768bf2a01d6f218ebc0
JL Yamin <jlyaminc@gmail.com>
2023-05-11 14:16:14 -0300

    fix(root): pR suggestions

--- packages/styles/src/lib/Theme/components/MuiButton.ts
+++ packages/styles/src/lib/Theme/components/MuiButton.ts
@@ -1,22 +1,29 @@
-import { Components } from '@mui/material';
+import { Components, Theme } from '@mui/material';
 import { palette as themePalette } from '../palette';
 import { addTransparency } from '../util';
 
-const selectColor = (color?: string, isHover?: boolean): string | undefined => {
+const selectColor = (
+  theme?: Theme,
+  color?: string,
+  isHover?: boolean
+): string | undefined => {
   switch (color) {
     case 'primary':
-      return isHover ? themePalette.primary?.dark : themePalette.primary?.main;
+      return isHover
+        ? theme?.palette.primary?.dark
+        : theme?.palette.primary?.main;
     case 'success':
     case 'error':
-      return themePalette[color]?.dark;
+      return theme?.palette[color]?.dark;
     case 'secondary':
-      return themePalette.grayScale?.blackHole;
+      return theme?.palette.grayScale?.blackHole;
     default:
-      return themePalette.grayScale?.eclipse;
+      return theme?.palette.grayScale?.eclipse;
   }
 };
 
 const selectIconColor = (
+  theme?: Theme,
   color?: string,
   disabled?: boolean,
   variant?: string
@@ -24,46 +31,64 @@ const selectIconColor = (
   return disabled
     ? 'inherit'
     : variant === 'outlined'
-    ? selectColor(color)
+    ? selectColor(theme, color)
     : themePalette.grayScale?.supernova;
 };
 
 export const MuiButton: Components['MuiButton'] = {
   styleOverrides: {
-    root: ({ ownerState }) => ({
-      padding: '8px 16px',
-      borderRadius: '2px',
-      boxShadow: 'unset',
-      textTransform: 'unset',
-      minHeight: '36px',
-      lineHeight: '20px',
-      fontWeight: '600',
-      transition: '0.3s',
-      '&:hover': {
+    root: (props) => {
+      const { ownerState } = props;
+      const theme = props.theme as Theme;
+      return {
+        padding: '8px 16px',
+        borderRadius: '2px',
         boxShadow: 'unset',
-        backgroundColor: selectColor(ownerState.color, true),
-      },
-      '.MuiButton-endIcon': {
-        marginRight: '-8px',
-      },
-      '.MuiButton-startIcon': {
-        marginLeft: '-8px',
-      },
-    }),
-    outlined: ({ ownerState }) => ({
-      borderColor: addTransparency(selectColor(ownerState.color)),
-      color: selectColor(ownerState.color),
-      '&:hover': {
-        borderColor: addTransparency(selectColor(ownerState.color, true)),
-        backgroundColor: addTransparency(selectColor(ownerState.color, true)),
-      },
-    }),
-    text: ({ ownerState }) => ({
-      color: selectColor(ownerState.color),
-      '&:hover': {
-        backgroundColor: addTransparency(selectColor(ownerState.color, true)),
-      },
-    }),
+        textTransform: 'unset',
+        minHeight: '36px',
+        lineHeight: '20px',
+        fontWeight: '600',
+        transition: '0.3s',
+        '&:hover': {
+          boxShadow: 'unset',
+          backgroundColor: selectColor(theme, ownerState.color, true),
+        },
+        '.MuiButton-endIcon': {
+          marginRight: '-8px',
+        },
+        '.MuiButton-startIcon': {
+          marginLeft: '-8px',
+        },
+      };
+    },
+    outlined: (props) => {
+      const { ownerState } = props;
+      const theme = props.theme as Theme;
+      return {
+        borderColor: addTransparency(selectColor(theme, ownerState.color)),
+        color: selectColor(theme, ownerState.color),
+        '&:hover': {
+          borderColor: addTransparency(
+            selectColor(theme, ownerState.color, true)
+          ),
+          backgroundColor: addTransparency(
+            selectColor(theme, ownerState.color, true)
+          ),
+        },
+      };
+    },
+    text: (props) => {
+      const { ownerState } = props;
+      const theme = props.theme as Theme;
+      return {
+        color: selectColor(theme, ownerState.color),
+        '&:hover': {
+          backgroundColor: addTransparency(
+            selectColor(theme, ownerState.color, true)
+          ),
+        },
+      };
+    },
     sizeSmall: {
       minHeight: '30px',
       padding: '5px 13px',
@@ -78,19 +103,29 @@ export const MuiButton: Components['MuiButton'] = {
       minHeight: '40px',
       padding: '10px 18px',
     },
-    endIcon: ({ ownerState }) => ({
-      color: selectIconColor(
-        ownerState.color,
-        ownerState.disabled,
-        ownerState.variant
-      ),
-    }),
-    startIcon: ({ ownerState }) => ({
-      color: selectIconColor(
-        ownerState.color,
-        ownerState.disabled,
-        ownerState.variant
-      ),
-    }),
+    endIcon: (props) => {
+      const { ownerState } = props;
+      const theme = props.theme as Theme;
+      return {
+        color: selectIconColor(
+          theme,
+          ownerState.color,
+          ownerState.disabled,
+          ownerState.variant
+        ),
+      };
+    },
+    startIcon: (props) => {
+      const { ownerState } = props;
+      const theme = props.theme as Theme;
+      return {
+        color: selectIconColor(
+          theme,
+          ownerState.color,
+          ownerState.disabled,
+          ownerState.variant
+        ),
+      };
+    },
   },
 };

3471a6faaf54a80a47c5a91b5d0baee8eecb882e
JL Yamin <jlyaminc@gmail.com>
2023-05-24 18:02:54 -0300

    wip: radioGroupField added elements

--- packages/radio-group-field/src/lib/radio-group-field.tsx
+++ packages/radio-group-field/src/lib/radio-group-field.tsx
@@ -1,10 +1,33 @@
 import MuiRadioGroup, { RadioGroupProps } from '@mui/material/RadioGroup';
+import MuiFormControl from '@mui/material/FormControl';
+import MuiFormLabel from '@mui/material/FormLabel';
+import MuiFormControlLabel from '@mui/material/FormControlLabel';
+import { Radio } from '@novatics/radio';
 
-interface RadioGroupFieldProps extends RadioGroupProps {}
+interface RadioGroupFieldProps extends RadioGroupProps {
+  label?: string;
+  options?: Array<{ label: string; value: string; disabled?: boolean }>;
+}
 
-const RadioGroupField = (props: RadioGroupFieldProps) => (
-  <MuiRadioGroup {...props} />
-);
+const RadioGroupField = (props: RadioGroupFieldProps) => {
+  const { label, children, options, ...others } = props;
+  return (
+    <MuiFormControl>
+      <MuiFormLabel>{label}</MuiFormLabel>
+      <MuiRadioGroup {...others}>
+        {options?.map((option) => (
+          <MuiFormControlLabel
+            value={option.value}
+            control={<Radio />}
+            label={option.label}
+            disabled={option.disabled}
+          />
+        ))}
+        {children}
+      </MuiRadioGroup>
+    </MuiFormControl>
+  );
+};
 
 export { RadioGroupField };
 export default RadioGroupField;

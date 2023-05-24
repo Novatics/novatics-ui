5645d6f2b3c9ba15d7917026f27741d4f76ed722
Matheus Gonçalves <souzagonc@gmail.com>
2023-05-24 12:48:22 -0300

    chore(root): remove storybook warning

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,7 +4,6 @@ module.exports = {
     '@storybook/addon-essentials',
     '@react-theming/storybook-addon',
     'storybook-dark-mode',
-    '@storybook/addon-mdx-gfm',
     '@storybook/addon-styling',
   ],
   typescript: {

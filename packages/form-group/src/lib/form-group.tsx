cb4541caad80ff214ff59b651c9a67f7f2262319
JL Yamin <jlyaminc@gmail.com>
2023-05-26 17:27:57 -0300

    feat(form-group): added childrenArray to FormGroup

--- packages/form-group/src/lib/form-group.tsx
+++ packages/form-group/src/lib/form-group.tsx
@@ -2,12 +2,16 @@ import { Stack, Typography } from '@mui/material';
 import MuiFormGroup, {
   FormGroupProps as MUIFormGroupProps,
 } from '@mui/material/FormGroup';
+import { Children, ReactElement, cloneElement, useMemo } from 'react';
 
-export interface FormGroupProps extends MUIFormGroupProps {
+export interface FormGroupProps extends Omit<MUIFormGroupProps, 'children'> {
   title?: string;
   direction?: 'row' | 'column';
   titleSpacing?: number;
   childrenSpacing?: number;
+  loading?: boolean;
+  disabled?: boolean;
+  children?: (() => React.ReactNode) | React.ReactNode;
 }
 
 const FormGroup = (props: FormGroupProps) => {
@@ -16,8 +20,30 @@ const FormGroup = (props: FormGroupProps) => {
     direction = 'column',
     titleSpacing = 2.5,
     childrenSpacing = 2.5,
+    children,
+    loading,
+    disabled,
+    ...others
   } = props;
-  console.log('props', props);
+
+  const childrenArray = useMemo(() => {
+    if (children) {
+      const executedChildren =
+        typeof children === 'function' ? children() : children;
+
+      return Children.toArray(executedChildren).map((child) => {
+        return typeof child === 'object'
+          ? cloneElement(child as ReactElement, {
+              loading,
+              disabled,
+              direction,
+            })
+          : child;
+      });
+    }
+
+    return [];
+  }, [children, direction, disabled, loading]);
 
   return (
     <Stack spacing={titleSpacing}>
@@ -29,8 +55,9 @@ const FormGroup = (props: FormGroupProps) => {
       <MuiFormGroup
         sx={{ gap: childrenSpacing }}
         row={direction === 'row'}
-        {...props}
-      />
+        {...others}
+      ></MuiFormGroup>
+      {childrenArray}
     </Stack>
   );
 };

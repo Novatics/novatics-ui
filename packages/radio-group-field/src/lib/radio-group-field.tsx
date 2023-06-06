75e4269984ccbc40e7f6cf3ca20b2268f0928dda
JL Yamin <jlyaminc@gmail.com>
2023-06-06 15:36:58 -0300

    feat(radio-group-field): finishing radio group

--- packages/radio-group-field/src/lib/radio-group-field.tsx
+++ packages/radio-group-field/src/lib/radio-group-field.tsx
@@ -2,6 +2,9 @@ import MuiRadioGroup, { RadioGroupProps } from '@mui/material/RadioGroup';
 import MuiFormControl from '@mui/material/FormControl';
 import MuiFormLabel from '@mui/material/FormLabel';
 import MuiFormControlLabel from '@mui/material/FormControlLabel';
+import FormHelperText, {
+  FormHelperTextProps,
+} from '@mui/material/FormHelperText';
 import { Radio } from '@novatics/radio';
 import { styled } from '@novatics/styles';
 
@@ -12,11 +15,17 @@ export interface RadioGroupFieldProps extends RadioGroupProps {
   loading?: boolean;
   error?: string | boolean;
   required?: boolean;
+  hint?: FormHelperTextProps['children'];
+  direction?: 'row' | 'column';
+  labelPlacement?: 'end' | 'start' | 'top' | 'bottom';
+  withTextHelper?: boolean;
 }
 
-const FormControl = styled(MuiFormControl)<RadioGroupProps>(() => ({
+const FormControl = styled(MuiFormControl)<{
+  direction?: 'row' | 'column';
+}>(({ direction }) => ({
   '.MuiFormGroup-root': {
-    flexDirection: 'row',
+    flexDirection: direction === 'row' ? 'row' : 'column',
     gap: '0 24px',
   },
   '.MuiFormLabel-root': {
@@ -33,24 +42,50 @@ const FormControl = styled(MuiFormControl)<RadioGroupProps>(() => ({
 }));
 
 const RadioGroupField = (props: RadioGroupFieldProps) => {
-  const { label, children, options, disabled, loading, ...others } = props;
+  const {
+    label,
+    children,
+    direction,
+    required,
+    options,
+    disabled,
+    loading,
+    hint,
+    error,
+    labelPlacement = 'end',
+    withTextHelper = true,
+    ...others
+  } = props;
 
   const disableRadio = disabled || loading;
 
+  let helperText = hint;
+
+  if (error && typeof error != 'boolean') {
+    helperText = error;
+  }
+
   return (
-    <FormControl required={props.required}>
+    <FormControl direction={direction} required={required}>
       {label && <MuiFormLabel>{label}</MuiFormLabel>}
       <MuiRadioGroup {...others}>
         {options?.map((option) => (
           <MuiFormControlLabel
+            key={option.value}
             value={option.value}
             control={<Radio />}
             label={option.label}
+            labelPlacement={labelPlacement}
             disabled={option.disabled || disableRadio}
           />
         ))}
         {children}
       </MuiRadioGroup>
+      {withTextHelper || helperText ? (
+        <FormHelperText error={Boolean(error)} sx={{ minHeight: '17px' }}>
+          {helperText}
+        </FormHelperText>
+      ) : null}
     </FormControl>
   );
 };

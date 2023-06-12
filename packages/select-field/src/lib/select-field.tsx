583f2db8ee3d9cca1addc97aedcb603005f4cf57
JL Yamin <jlyaminc@gmail.com>
2023-06-12 20:42:49 -0300

    feat(select-field): added formik to select

--- packages/select-field/src/lib/select-field.tsx
+++ packages/select-field/src/lib/select-field.tsx
@@ -2,14 +2,26 @@ import { FormControl, FormLabel } from '@mui/material';
 import MuiSelect, { SelectProps } from '@mui/material/Select';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
-const SelectField = (props: SelectProps) => {
-  const { label, required, ...others } = props;
+export interface SelectFieldProps extends Omit<SelectProps, 'error'> {
+  label?: string;
+  options?: Array<{ label: string; value: string; disabled?: boolean }>;
+  disabled?: boolean;
+  loading?: boolean;
+  error?: string | boolean;
+  required?: boolean;
+}
+
+const SelectField = (props: SelectFieldProps) => {
+  const { label, required, error, disabled, loading, ...others } = props;
+
+  const disableSelect = disabled || loading;
+
   return (
     <FormControl>
       <FormLabel required={required} focused={false}>
         {label}
       </FormLabel>
-      <MuiSelect IconComponent={ExpandMoreIcon} {...others} />
+      <MuiSelect IconComponent={ExpandMoreIcon} disabled={disableSelect} error={Boolean(error)} {...others} />
     </FormControl>
   );
 };

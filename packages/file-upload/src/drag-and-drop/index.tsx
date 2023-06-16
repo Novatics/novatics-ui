1c7de8f2d9d771680ebf979c7bd9e3bd1d5cafa9
JL Yamin <jlyaminc@gmail.com>
2023-06-16 18:35:24 -0300

    feat(file-upload): both variants styling done

--- packages/file-upload/src/drag-and-drop/index.tsx
+++ packages/file-upload/src/drag-and-drop/index.tsx
@@ -1,10 +1,18 @@
+import { useCallback, useRef, useState } from 'react';
 import { UploadFile } from '@mui/icons-material';
-import { Box } from '@mui/material';
-import { styled } from '@novatics/styles';
-
-export type DragAndDropVariant = 'inline' | 'fullscreen';
+import { ButtonProps } from '@novatics/button';
+import {
+  Button,
+  DragAndDropContainer,
+  ErrorText,
+  FullscreenFormatContainer,
+  FullscreenFormatHint,
+  FullscreenFormats,
+  InlineFormatHint,
+  Title,
+} from './styles';
 export interface DragAndDropProps {
-  accept: string[];
+  accept: string;
   acceptedLabels?: string;
   onDrop: (files: File[]) => void;
   onDragEnter?: (event: React.DragEvent<HTMLDivElement>) => void;
@@ -13,7 +21,7 @@ export interface DragAndDropProps {
   onDropAccepted?: (event: React.DragEvent<HTMLDivElement>) => void;
   onDropRejected?: (event: React.DragEvent<HTMLDivElement>) => void;
   onFileDialogCancel?: (event: React.DragEvent<HTMLDivElement>) => void;
-  onFileDialogOpen?: (event: React.DragEvent<HTMLDivElement>) => void;
+  onFileDialogOpen?: (event: React.MouseEvent<HTMLInputElement>) => void;
   noClick?: boolean;
   noDrag?: boolean;
   disabled?: boolean;
@@ -21,27 +29,133 @@ export interface DragAndDropProps {
   buttonLabel?: string;
   acceptedFormatsLabel?: string;
   uploadIcon?: React.ReactNode;
-  variant?: DragAndDropVariant;
+  variant?: 'inline' | 'fullscreen';
+  buttonProps?: ButtonProps;
+  inputProps?: React.InputHTMLAttributes<HTMLInputElement>;
+  error?: string;
 }
-
-export const DragAndDropContainer = styled(Box)<{
-  variant?: DragAndDropVariant;
-}>((props) => {
-  return {
-    border: '1px dashed',
-    borderRadius: '2px',
-    borderColor: props.theme.palette.grey[50],
-    minWidth: '600px',
-  };
-});
 
 export function DragAndDrop(props: DragAndDropProps) {
   const {
     onDrop,
+    onDragOver,
+    onDragLeave,
+    onFileDialogOpen,
+    accept,
+    error,
+    disabled,
     title = 'Drag and drop files here',
     buttonLabel = 'Select files',
-    acceptedFormatsLabel = 'Accepted formats',
+    acceptedFormatsLabel = 'Accepted formats:',
+    acceptedLabels,
+    variant = 'inline',
     uploadIcon = <UploadFile />,
+    buttonProps,
+    inputProps,
   } = props;
-  return <DragAndDropContainer>{uploadIcon}</DragAndDropContainer>;
+
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [dragOver, setDragOver] = useState(false);
+  const isFullscreen = variant === 'fullscreen';
+
+  const formatsHint = () =>
+    isFullscreen ? (
+      <FullscreenFormatContainer>
+        <FullscreenFormatHint variant="overline" $disabled={disabled}>
+          {acceptedFormatsLabel}
+        </FullscreenFormatHint>
+        <FullscreenFormats variant="caption" $disabled={disabled}>
+          {acceptedLabels}
+        </FullscreenFormats>
+      </FullscreenFormatContainer>
+    ) : error ? (
+      <ErrorText variant="caption">{error}</ErrorText>
+    ) : (
+      <InlineFormatHint variant="caption" $disabled={disabled}>
+        {acceptedFormatsLabel} {acceptedLabels}
+      </InlineFormatHint>
+    );
+
+  const handleDrop = useCallback(
+    (e: React.DragEvent<HTMLDivElement>) => {
+      e.preventDefault();
+      if (disabled) return;
+      if (onDrop) onDrop(Array.from(e.dataTransfer.files));
+      setDragOver(false);
+    },
+    [onDrop, disabled]
+  );
+
+  const handleDragOver = useCallback(
+    (e: React.DragEvent<HTMLDivElement>) => {
+      e.preventDefault();
+      if (disabled) return;
+      e.stopPropagation();
+      if (onDragOver) onDragOver(e);
+      setDragOver(true);
+    },
+    [onDragOver, disabled]
+  );
+
+  const handleDragLeave = useCallback(
+    (e: React.DragEvent<HTMLDivElement>) => {
+      e.preventDefault();
+      if (disabled) return;
+      e.stopPropagation();
+      if (onDragLeave) onDragLeave(e);
+      setDragOver(false);
+    },
+    [onDragLeave, disabled]
+  );
+
+  const handleButton = () => {
+    if (disabled) return;
+    if (inputRef.current) inputRef.current.click();
+  };
+
+  const handleOpenDialog = (e: React.MouseEvent<HTMLInputElement>) => {
+    if (onFileDialogOpen) onFileDialogOpen(e);
+  };
+
+  return (
+    <DragAndDropContainer
+      variant={variant}
+      onDrop={handleDrop}
+      onDragOver={handleDragOver}
+      onDragLeave={handleDragLeave}
+      $isHovering={dragOver}
+      $error={Boolean(error)}
+      $disabled={disabled}
+    >
+      <input
+        type="file"
+        accept={accept}
+        ref={inputRef}
+        onClick={handleOpenDialog}
+        style={{ display: 'none' }}
+        {...inputProps}
+      />
+      {uploadIcon}
+      <Title variant={isFullscreen ? 'h5' : 'body'} $disabled={disabled}>
+        {title}
+      </Title>
+      {formatsHint()}
+      {isFullscreen && (
+        <ErrorText containerVariant={variant} variant="caption">
+          {error}
+        </ErrorText>
+      )}
+      <Button
+        onClick={handleButton}
+        color={error ? 'error' : 'primary'}
+        size={isFullscreen ? 'large' : 'small'}
+        variant="outlined"
+        disabled={disabled}
+        containerVariant={variant}
+        {...buttonProps}
+      >
+        {buttonLabel}
+      </Button>
+    </DragAndDropContainer>
+  );
 }

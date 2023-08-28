3a1f36c2f56684a39d10c33433543c72d8ffe8a8
Matheus Gonçalves <souzagonc@gmail.com>
2023-08-28 17:32:14 -0300

    feat(root): adjust padding of select

--- packages/styles/src/lib/Theme/components/MuiSelect.ts
+++ packages/styles/src/lib/Theme/components/MuiSelect.ts
@@ -3,7 +3,7 @@ import { Components } from '@mui/material';
 export const MuiSelect: Components['MuiSelect'] = {
   styleOverrides: {
     select: {
-      padding: '16.5px 0px',
+      padding: '0px',
     },
     icon: {
       width: '0.95rem',
